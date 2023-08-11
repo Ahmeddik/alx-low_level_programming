d86e5fe5f5edd7d682b7c31698e88b17d762acc3
Ahmeddik <ahmeddik22@gmail.com>
2023-08-11 06:47:04 -0700

    c is fun

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,10 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	for (i = 0; i < 52; i++){
+	for (i = 0; i < 52; i++)
+	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
